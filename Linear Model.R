819cc5368163100b757750911b298f67fbad05f0
statistic-ian <52239370+statistic-ian@users.noreply.github.com>
2021-06-11 14:26:17 +0100

    Add files via upload

--- Linear Model.R
+++ Linear Model.R
@@ -22,8 +22,8 @@ df
 
 makeX_lm <- function(df){
   X_lm <- matrix(0,
-              nrow(df),
-              length(levels(factor(as.character(df$TeamName1)))))
+                 nrow(df),
+                 length(levels(factor(as.character(df$TeamName1)))))
   colnames(X_lm) <- levels(factor(as.character(df$TeamName1)))
   for (team in colnames(X_lm)) {
     X_lm[df$TeamName1 == team, team] <- 1
@@ -39,56 +39,15 @@ df$X <- makeX_lm(df)
 
 # Fit linear model ---------------------------------------
 
-lm_fit <- lm(logit_p_ij ~ -1 + X, data = df)
+lmfi_groupo <- lm(logit_p_ij ~ -1 + X, data = df)
 
-r <- coef(lm_fit)
+r <- coef(lmfit_group)
 r[is.na(r)] <- 0
 r
+s <- exp(r)
 
 
-# Check that setting to zero is correct thing to do --------------
-
-#Looking at just Group A and going back to basics on linear regression
-
-df_A <- subset(df, Group == "A",)
-
-df_A$X <- makeX_lm(df_A)
-
-X <- df_A$X
-
-XTX <- t(X)%*%X
-
-y <- df_A$logit_p_ij
-
-XTy <- t(X)%*%y
-
-## XTX is not of full rank so replace first line with identifiability constraint
-
-XTX[1,] <- c(1,1,1,1)
-
-XTy[1] <- 0
-
-s_A <- solve(XTX, XTy)
-
-s_A <- s_A - s_A[4]
-s_A # these match what we got from lmfit
-
-## Just to check if truly colinear then shouldn't matter which row the identifiability constraint is imposed on
-
-XTX <- t(X)%*%X
-
-XTy <- t(X)%*%y
-
-XTX[4,] <- c(1,1,1,1)
-
-XTy[4] <- 0
-
-s_A <- solve(XTX, XTy)
-
-s_A <- s_A - s_A[4]
-s_A # these match what we got from lmfit with NAs subbed to 0 and from previous calc - we are all good!
-
-# ----------------------------------------------------------------
+# Calc probs ------ leaving code here for now but to be moved -----
 
 r <- setNames(r, colnames(df$X))
 probs <- plogis(outer(r, r, '-'))
@@ -100,5 +59,51 @@ probs
 
 # scale the Group probabilities ----------------------------
 
+# Under Bradley-Terry the probability of i winning the final given i has made it to the final is 
+# q_io = \pi_i / (\pi_i + \pi_o) where \pi_i is the strength of team i and \pi_o is the strength of the final opponent.
+# \pi_o is assumed to be the same for all teams (since there are so many permutations of reaching final)
+# so logit(q_io) = lambda_i - lambda_o (1) where lambda_i = log(\pi_i).
+# We have the intra-group strengths s_i, but we do not know the relative strengths of the groups.
+# Suppose group G has strength \gamma_G such that s_i = \gamma_G(i) \pi_i where G(i) is i's group.
+# Then lambda_i = log(\pi_i) = log(s_i) - log(\gamma_G(i)) = r_i - lambda_G(i) (2) where lambda_G(i) = log(\gamma_G(i))
+# So combining (1) and (2) we have r_i - logit(q_io) = lambda_G(i) + \lambda_o.
+# So we estimate \lambda_o and the \lambda_G by regressing against r_i - logit(q_io) 
+
+
+# take tournament win odds and translate to tournament win odds
+
+winner <- read_csv("Winner.csv")%>%
+  mutate(p_i = 1 / Odds)
+total_prob <- sum(winner$p_i)
+winner$p_i <- winner$p_i/total_prob
+
+# take tournament 'reaching final' odds and translate to 'tournament 'reaching final' odds 
+
+final <- read_csv("making_final.csv")%>%
+  mutate(p_i = 1 / Odds)
+total_prob <- sum(final$p_i)
+final$p_i <- 2*final$p_i/total_prob
+
+# calculate the conditional probability of winning given you make the final
+
+winner$q_io <- winner$p_i/final$p_i
+
+y <- r - qlogis(winner$q_io)
+
+X <- matrix(0,
+               nrow(winner),
+               length(levels(factor(as.character(winner$Group)))))
+colnames(X) <- levels(factor(as.character(winner$Group)))
+rownames(X) <- winner$TeamName
+for (group in colnames(X)) {
+  X[winner$Group == group, group] <- 1
+}
+X
+
+lmfit_win <- lm(y ~ X)
+
+lambda <- coef(lmfit_win)
+lambda[is.na(lambda)] <- 0
+lambda
 
 
